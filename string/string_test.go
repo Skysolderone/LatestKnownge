acad3c65f7e9b9146711b2dc13d4fb5d993e97e1
wws <wws741@qq.com>
2023-12-11 18:34:40 +0800

    1211

---
+++ string/string_test.go
@@ -0,0 +1,90 @@
+package test
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"strings"
+	"testing"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func randomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+// +
+func plusConcat(n int, str string) string {
+	s := ""
+	for i := 0; i < n; i++ {
+		s += str
+	}
+	return s
+}
+
+// fmt.Sprintf
+func sprintfConcat(n int, str string) string {
+	s := ""
+	for i := 0; i < n; i++ {
+		s = fmt.Sprintf("%s%s", s, str)
+	}
+	return s
+}
+
+// strings.Builder
+func builderConcat(n int, str string) string {
+	var builder strings.Builder
+	for i := 0; i < n; i++ {
+		builder.WriteString(str)
+	}
+	return builder.String()
+}
+
+// bytes.Buffer
+func bufferConcat(n int, s string) string {
+	buf := new(bytes.Buffer)
+	for i := 0; i < n; i++ {
+		buf.WriteString(s)
+	}
+	return buf.String()
+}
+
+// []byte
+func byteConcat(n int, str string) string {
+	buf := make([]byte, 0)
+	for i := 0; i < n; i++ {
+		buf = append(buf, str...)
+	}
+	return string(buf)
+}
+
+// []byte 20
+func preByteConcat(n int, str string) string {
+	buf := make([]byte, 0, n*len(str))
+	for i := 0; i < n; i++ {
+		buf = append(buf, str...)
+	}
+	return string(buf)
+}
+
+func benchmark(b *testing.B, f func(int, string) string) {
+	var str = randomString(10)
+	for i := 0; i < b.N; i++ {
+		f(10000, str)
+	}
+}
+
+func BenchmarkPlusConcat(b *testing.B)    { benchmark(b, plusConcat) }
+func BenchmarkSprintfConcat(b *testing.B) { benchmark(b, sprintfConcat) }
+func BenchmarkBuilderConcat(b *testing.B) { benchmark(b, builderConcat) }
+func BenchmarkBufferConcat(b *testing.B)  { benchmark(b, bufferConcat) }
+func BenchmarkByteConcat(b *testing.B)    { benchmark(b, byteConcat) }
+func BenchmarkPreByteConcat(b *testing.B) { benchmark(b, preByteConcat) }
+
+//go test -bench="Concat$" -benchmem .
+//推荐使用strings.Builder
