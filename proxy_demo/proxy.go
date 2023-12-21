4e34c46be8ad841f770d668ba9e900f11d8cf98f
wws <wws741@qq.com>
2023-12-21 22:02:44 +0800

    1221

--- proxy_demo/proxy.go
+++ proxy_demo/proxy.go
@@ -8,32 +8,33 @@ import (
 
 func handle(conn net.Conn) {
 	defer conn.Close()
+	// target, err := net.Dial("tcp", "8.218.201.224:80")
 	target, err := net.Dial("tcp", "192.168.10.128:80")
 	if err != nil {
-		log.Println(err)
+		log.Println("connect:", err)
 		return
 	}
 	defer target.Close()
 	go func() {
 		_, err := io.Copy(target, conn)
 		if err != nil {
-			log.Println(err)
+			log.Println("copy:", err)
 		}
 	}()
 	_, err = io.Copy(conn, target)
 	if err != nil {
-		log.Println(err)
+		log.Println("copy2:", err)
 	}
 }
 func main() {
 	listen, err := net.Listen("tcp", "127.0.0.1:55")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("listen:", err)
 	}
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
-			log.Println(err)
+			log.Println("accept:", err)
 			continue
 		}
 		go handle(conn)
