d25f4ef1efc9c1972282244ad408f729143b719e
wws <wws741@qq.com>
2024-01-17 09:46:09 +0800

    0117

---
+++ plugin_demo/plugin0117.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+/*
+load plugin
+plug, err := plugin.Open("./plugin.dll")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+find and return addr
+symplugin, err := plug.Lookup("symbolName")
+
+unload plugin
+plug.Close()
+*/
+type Calculator interface {
+	Add(a, b int) int
+	Sub(a, b int) int
+}
+type MyCalculator struct{}
+
+func (m *MyCalculator) Add(a, b int) int {
+	return a + b
+}
+func (m *MyCalculator) Sub(a, b int) int {
+	return a - b
+}
+//go build -buildmode=plugin -o myplugin.so main.go
+func main() {
+	fmt.Println("Hello plugin")
+	var c Calculator
+	if c != nil {
+		fmt.Println(c.Add(1, 2))
+		fmt.Println(c.Sub(2, 1))
+	}
+}
