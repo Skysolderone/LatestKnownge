e0c62a7f3e26bc6e8a412fd57e24cdcb9cad2a45
wws <wws741@qq.com>
2024-02-27 16:41:50 +0800

    0227

--- 0227.go
+++ 0227.go
@@ -1,19 +1,27 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"strings"
+)
 
 var m = make(map[string]string)
 
 func main() {
-	go func() {
-		for {
-			_ = m["x"]
-		}
-	}()
-	go func() {
-		for {
-			m["y"] = "foo"
-		}
-	}()
-	time.Sleep(1 * time.Second)
+	sort := "vip desc"
+	switch {
+	case strings.Contains(sort, "total_money") && strings.Contains(sort, "asc"):
+		fmt.Println("1")
+	case strings.Contains(sort, "total_money") && strings.Contains(sort, "desc"):
+		fmt.Println("2")
+	case strings.Contains(sort, "vip") && strings.Contains(sort, "asc"):
+		fmt.Println("3")
+	case strings.Contains(sort, "vip") && strings.Contains(sort, "desc"):
+		fmt.Println("4")
+	case strings.Contains(sort, "money") && strings.Contains(sort, "asc"):
+		fmt.Println("5")
+	case strings.Contains(sort, "money") && strings.Contains(sort, "desc"):
+		fmt.Println("6")
+
+	}
 }
