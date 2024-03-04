45450b5d8170e8ce6f910f391ff48393190968ba
wws <wws741@qq.com>
2024-03-04 15:36:09 +0800

    0304

---
+++ dns_demo/dns0304.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/miekg/dns"
+)
+
+func resolve(domain string, qtype uint16) []dns.RR {
+	m := new(dns.Msg)
+	m.SetQuestion(dns.Fqdn(domain), qtype)
+	m.RecursionDesired = true
+	c := &dns.Client{Timeout: 5 * time.Second}
+	response, _, err := c.Exchange(m, "8.8.8.8:53")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if response == nil {
+		log.Fatal("no response ")
+	}
+	for _, answer := range response.Answer {
+		fmt.Printf("%s\n", answer.String())
+	}
+	return response.Answer
+}
+
+func main() {
+	domain := "www.baidu.com"
+	resolve(domain, dns.TypeA)
+}
