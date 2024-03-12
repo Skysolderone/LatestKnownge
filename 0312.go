c762f470873832ba281761c144fa07068ca36d39
wws <wws741@qq.com>
2024-03-12 10:35:57 +0800

    0312

--- 0312.go
+++ 0312.go
@@ -10,4 +10,11 @@ func main() {
 	nice := time.Since(time.Now().Truncate(time.Hour))
 	log.Println(diff)
 	log.Println(nice)
+
+	timer := time.NewTicker(time.Second * 3)
+	for range timer.C {
+		log.Println(3)
+		timer.Reset(time.Second * 5)
+
+	}
 }
